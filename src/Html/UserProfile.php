c29e3498ce43eea1c0463a2f1eb2b17170e42ea8
Thomas Denoyelle <thomas.denoyelle@etudiant.univ-reims.fr>
2025-09-30 23:01:07 +0200

    Ajout du stringEscaper et du UserProfile

---
+++ src/Html/UserProfile.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Html;
+
+use Entity\User;
+
+class UserProfile
+{
+    use StringEscaper;
+    protected User $user;
+
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+
+    public function toHtml(): string
+    {
+        $html = <<<HTML
+<p>Nom : {$this->escapeString((string) $this->user->getLastName())}</p>
+<p>Prénom : {$this->escapeString((string) $this->user->getFirstName())}</p>
+<p>Login : {$this->escapeString((string) $this->user->getLogin())}</p>
+<p>Téléphone : {$this->escapeString((string) $this->user->getPhone())}</p>
+HTML;
+
+        return $html;
+    }
+}
