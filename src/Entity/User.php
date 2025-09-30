ac393d4ae7a50b2f87712ee21a9e19f9b797a15b
Thomas Denoyelle <thomas.denoyelle@etudiant.univ-reims.fr>
2025-09-30 22:41:56 +0200

    Ajout de la classe User et de ses attributs et getters.

---
+++ src/Entity/User.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Entity;
+
+class User
+{
+    private int $id;
+
+    private string $firstName;
+
+    private string $lastName;
+
+    private string $email;
+
+    private string $login;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLogin(): string
+    {
+        return $this->login;
+    }
+
+}
