a0fa8eb9511cfd001094938dde79d13497b69fbe
ToMaTo1206 <114519489+ToMaTo1206@users.noreply.github.com>
2025-10-05 16:30:41 +0200

    Ajout des propriétés de la classe Note

--- src/Entity/Note.php
+++ src/Entity/Note.php
@@ -4,5 +4,55 @@ namespace Entity;
 
 class Note
 {
+    private int $noteId;
+
+    private string $title;
+    private string $content;
+
+    private string $createdAt;
+
+    private string $updatedAt;
+
+    /**
+     * @return int
+     */
+    public function getNoteId(): int
+    {
+        return $this->noteId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCreatedAt(): string
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUpdatedAt(): string
+    {
+        return $this->updatedAt;
+    }
+
+
 
 }
