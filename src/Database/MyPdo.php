0740c55e465e33e034f757c0df025700fdd5fb67
ToMaTo1206 <114519489+ToMaTo1206@users.noreply.github.com>
2025-10-05 16:08:52 +0200

    MAJ pour intéragir avec la bd phpmyamdin

--- src/Database/MyPdo.php
+++ src/Database/MyPdo.php
@@ -1,34 +1,17 @@
 <?php
 
 namespace Database;
-use PDO;
 
-class MyPdo extends PDO
+class MyPdo extends \PDO
 {
     private static ?self $instance = null;
 
-    private const DB_HOST = "localhost";
-    private const DB_NAME = "gestion_notes";   // adapte avec ta base
-    private const DB_USER = "root";            // par défaut sous XAMPP
-    private const DB_PASS = "";                // vide par défaut
-    private const DB_CHARSET = "utf8mb4";
-
-    private function __construct()
-    {
-        $dsn = "mysql:host=" . self::DB_HOST . ";dbname=" . self::DB_NAME . ";charset=" . self::DB_CHARSET;
-
-        parent::__construct($dsn, self::DB_USER, self::DB_PASS);
-
-        // Configuration PDO
-        $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $this->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-    }
-
     public static function getInstance(): self
     {
-        if (self::$instance === null) {
-            self::$instance = new self();
+        if (null === self::$instance) {
+            self::$instance = new self('mysql:host=127.0.0.1;dbname=symplyNote;charset=utf8mb4', 'root', '');
         }
+
         return self::$instance;
     }
 }
