2f1331c81bc6c79225f265b8af10348ab5395170
Thomas Denoyelle <thomas.denoyelle@etudiant.univ-reims.fr>
2025-10-06 23:19:12 +0200

    Page affichant toutes les notes de l'utilisateur connecté

---
+++ public/notes.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+use Authentication\Exception\NotLoggedInException;
+use Authentication\UserAuthentication;
+use Html\AppWebPage;
+use Service\Exception\SessionException;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$authentification = new UserAuthentication();
+
+try {
+    $authentification->logoutIfRequested();
+
+    if (!$authentification->isUserConnected()) {
+        header('Location: form.php');
+        exit;
+    }
+    $user = $authentification->getUser();
+} catch (SessionException|NotLoggedInException $e) {
+    header('Location: form.php');
+    exit;
+}
+
+$notes = \Entity\Collection\NoteCollection::findAllNotesFromUser($user->getId());
+
+$webPage = new AppWebPage('Liste des notes');
+
+$webPage->appendContent("<div class='listNotes'>");
+foreach ($notes as $note) {
+    $webPage->appendContent("<a class='note' href='note.php?id={$note->getId()}'>");
+    $webPage->appendContent("<p >{$note->getTitle()}</p>");
+    $webPage->appendContent("<p ></p>");
+    $webPage->appendContent("<p>{$note->getContent()}</p>");
+    $webPage->appendContent('</a>');
+}
+$webPage->appendContent('</div>');
+
+echo $webPage->toHTML();
