a0f5f84e552743a27c103737febb2593763318cf
Thomas Denoyelle <thomas.denoyelle@etudiant.univ-reims.fr>
2025-10-06 23:30:46 +0200

    MAJ du lien vers le css

--- src/Html/AppWebPage.php
+++ src/Html/AppWebPage.php
@@ -14,7 +14,7 @@ class AppWebPage extends WebPage
     public function __construct(string $title = '')
     {
         parent::__construct($title);
-        $this->appendCssUrl('/css/style.css');
+        $this->appendCssUrl('css/style.css');
     }
 
     /**
