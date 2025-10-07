5a1704c42ead3683ecb4abd6af1c3d6edf746a29
Thomas Denoyelle <thomas.denoyelle@etudiant.univ-reims.fr>
2025-10-07 22:15:23 +0200

    Bouton pour revenir aux notes et un autre pour modif la note

--- public/note.php
+++ public/note.php
@@ -31,6 +31,8 @@ $note = \Entity\Note::findNoteById((int) $_GET['id']);
 
 $webPage = new AppWebPage("{$note->getTitle()}");
 
+$webPage->appendMenu("<a href=\"notes.php\">Back to notes</a><a href='updateNote.php?id={$note->getId()}'>modifier note ?</a>");
+
 $webPage->appendContent("<p class='note'>");
 $webPage->appendContent("<p>{$note->getContent()}</p>");
 $webPage->appendContent('</p>');
